6659a67f27b0eb1164e8666bb52dc0666412f9db
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-19 13:54:00 -0400

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+
   root 'home#index'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
