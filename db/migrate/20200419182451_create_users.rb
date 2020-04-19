c50a456a007fab6ec79f227a01fc388ac147d760
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-19 14:48:59 -0400

    add users model

---
+++ db/migrate/20200419182451_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :email, :null => false
+      t.string :password_digest
+
+      t.belongs_to :team, :null => false
+
+      t.boolean :is_admin, :default => false
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_182050) do
+ActiveRecord::Schema.define(version: 2020_04_19_182451) do
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "password_digest"
+    t.integer "team_id", null: false
+    t.boolean "is_admin", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["team_id"], name: "index_users_on_team_id"
   end
 
 end
